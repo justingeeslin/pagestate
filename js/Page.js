6382caeb35d2f0e4d0fa4d259f1c2af40fb00ef2
Justin Geeslin <justin@geesl.in>
2017-08-10 15:53:16 -0500

    Updated page object. Title and Suffix. Postload callback. Scrolling

--- js/Page.js
+++ js/Page.js
@@ -4,11 +4,38 @@ const Component = require('sensible-component')
 var Page = function( options ) {
   var self = this;
 
+  // Title of the page. Appears in history, bookmarks, and tabs
+  var title = "";
+  // Suffix like the name of the app, etc.
+  var suffix = "";
+
+  Object.defineProperty(this, 'title', {
+    get: function() { return title; },
+    set: function(newTitle) {
+      title = newTitle;
+      document.title = title + this.suffix;
+      return true
+    },
+    enumerable: true
+  });
+
+  Object.defineProperty(this, 'suffix', {
+    get: function() { return suffix; },
+    set: function(newSuffix) {
+      suffix = newSuffix;
+      document.title = this.title + suffix;
+      return true
+    },
+    enumerable: true
+  });
+
   var defaults = {
-    // Title of the page. Appears in history, bookmarks, and tabs
-    title: "",
-    // Suffix like the name of the app, etc.
-    suffix: "",
+    // A special character after which in the state should be some selector for an element to scroll to
+    fragmentCharacter: "#",
+    // The current fragment last scrolled to
+    fragment: "",
+    // Customizable post-load function
+    postload: function() { }
   }
 
   self = extend(self, defaults);
@@ -16,18 +43,35 @@ var Page = function( options ) {
 
   self = extend(self, new Component(self));
 
-  var setTitle = function(title) {
-    self.log('Setting the title');
-    window.location.title = title + suffix;
+  $(this.target).on('postload.page', this.postload);
+
+  this.stateChange = function(oldState, newState) {
+    self.log('New state: ' + newState)
+    $(this.target).trigger('postload.page');
   }
 
-  this.stateChange = function(page) {
-    // Call parent go.
-    self._super.go()
+  // Process the fragment on the state
+  $(this.target).on('postload.page', function() {
+    self.fragment = self.state.split(self.fragmentCharacter)[1]
 
-    // Set title
-    setTitle(self.title)
-  }
+    if (typeof self.fragment === "undefined" || self.fragment.length <= 0) {
+      self.log('No fragment. Exiting.. State: ' + self.state)
+      return;
+    }
+
+    var scrollTarget = self.target.find('#' + self.fragment);
+
+    if (typeof scrollTarget.offset === "undefined") {
+			console.warn('Scrolling to a element that isnt in the DOM. Exiting..', scrollTarget);
+			return false;
+		}
+
+    var distanceToScroll = scrollTarget.offset().top;
+
+    $('html, body').scrollTop(distanceToScroll);
+  });
+
+  $(this.target).trigger('postload.page');
 
   return this;
 }
--- tests/Page.js
+++ tests/Page.js
@@ -12,4 +12,53 @@ describe('Page', function() {
 			expect(thePage instanceof Page).toBe(true)
     });
 
+		it('should set title on construction', function() {
+			var thePage = new Page({
+				title: "My First Page"
+			});
+
+			expect(document.title).toBe("My First Page")
+    });
+
+		it('should set suffix on construction', function() {
+			var thePage = new Page({
+				title: "My First Page",
+				suffix: " - A Sensible Application"
+			});
+
+			expect(document.title).toBe("My First Page - A Sensible Application")
+    });
+
+		it('should call a postload callback', function(done) {
+			isCalled = false;
+			var thePage = new Page({
+				postload: function() {
+					isCalled = true;
+					expect(isCalled).toBe(true)
+					done()
+				}
+			});
+
+    });
+
+		it('should scroll to an element when there is a fragment', function() {
+
+			$(document.body).append('<div id="content"></div>');
+
+			$('#content').append('<h1 id="one">Heading One</h1>')
+			$('#content').append('<h1 id="two">Heading Two</h1>')
+			// Make sure the page is scrollable
+			$('#content').append('<h1 id="three" style="margin-bottom:10000em;">Heading Three</h1>')
+
+			thePage = new Page({
+				target: $("#content"),
+				debug: true
+			});
+
+			thePage.state = 'contactus#two';
+
+			expect($(window).scrollTop()).not.toBe(0);
+
+    });
+
 });
